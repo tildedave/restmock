1467a4238053cb166c744a590f2b4cfc558b7b1f
Dave King <tildedave@gmail.com>
2011-04-14 21:23:05 -0400

    upgrade to 1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject restmock "1.0.0-SNAPSHOT"
+(defproject restmock "1.1.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
