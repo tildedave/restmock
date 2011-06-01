de0a41a06dbb812963c9987d35c401763ba1079f
Dave King <tildedave@gmail.com>
2011-05-31 21:01:24 -0400

    Update documentation.

--- example-config.clj
+++ example-config.clj
@@ -1,3 +1,19 @@
 (routes
- (route (request (uri "/foo"))
-        (response (text "Foo!"))))
+ (route "Hello, world!"
+        (request (uri "/hello"))
+        (response (text "Hello, world!")))
+ (route "Can retrieve all the kittens"
+        (request (uri "/kittens")
+                 (method :get))
+        (response (text "Some adorable kittens!")))
+ (route "Can't make a new kitten"
+        (request (uri "/kittens")
+                 (method :post))
+        (response (status 422)))
+ (route "Can update a kitten"
+        (request (uri "/kittens/([0-9]+)")
+                 (method :put))
+        (response (status 202)))
+ (route "Kitten XML"
+        (request (uri "/kittens/([0-9]+)"))
+        (response (xml-file "cute-kitten.xml"))))
